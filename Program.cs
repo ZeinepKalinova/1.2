1f53ab3cc15ac6524e0123e54aef297030c4f437
ZeinepKalinova <zeinepkalinova@mail.ru>
2023-10-23 10:07:02 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("ФИО: ");
+        string fullName = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(fullName))
+        {
+            string greeting = $"Hello {fullName}!";
+            Console.WriteLine(greeting);
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+    }
+}
+
